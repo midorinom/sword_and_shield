09fb8552f06b54a615b3a8d5e734e4f2a00a7b9e
haofang <haofang97@gmail.com>
2022-09-09 14:21:35 +0800

    improved Action Gauge logic + added comments

--- script.js
+++ script.js
@@ -1,27 +1,50 @@
 "use strict";
 
-let counter = 0;
-
+// Defining variables
+let playerActionGaugeCounter = 0;
 const playerActionGauge1 = document.querySelector(
   "#player_action_gauge1_progress"
 );
-
 const playerActionGauge2 = document.querySelector(
   "#player_action_gauge2_progress"
 );
 
 function fillActionGauge() {
-  playerActionGauge1.style.height = counter + "%";
-  counter += 1;
+  // Make the height of the progress bar correspond to the counter value, then increment counter
+  if (playerActionGaugeCounter < 100) {
+    playerActionGaugeCounter += 1;
+    playerActionGauge1.style.height = playerActionGaugeCounter + "%";
+  }
+  if (playerActionGaugeCounter >= 100 && playerActionGaugeCounter < 200) {
+    playerActionGaugeCounter += 1;
+    playerActionGauge2.style.height = playerActionGaugeCounter - 100 + "%";
+  }
 }
 
-function resetGauge() {
-  clearInterval(autoActionGauge);
-  counter = 0;
-  playerActionGauge1.style.height = 0;
-  autoActionGauge = setInterval(fillActionGauge, 20);
+// Start filling the Action Gauge
+let autoActionGauge = setInterval(fillActionGauge, 50);
+
+function executeAction() {
+  // Check first if there is at least 1 full bar of Action Gauge before executing the function
+  if (playerActionGaugeCounter >= 100) {
+    // Subtract 1 bar 's worth (100) from the counter
+    playerActionGaugeCounter -= 100;
+
+    // Adjust the height of the bars to reflect the deduction from the counter
+    // if the player had only 1 full bar initially:
+    if (playerActionGaugeCounter <= 100) {
+      playerActionGauge2.style.height = 0;
+      playerActionGauge1.style.height = playerActionGaugeCounter + "%";
+    }
+    // if the player had 2 full bars initially:
+    else {
+      playerActionGauge1.style.height = "100%";
+      playerActionGauge2.style.height = playerActionGaugeCounter - 100 + "%";
+    }
+  }
 }
 
-let autoActionGauge = setInterval(fillActionGauge, 20);
-
-document.querySelector("#reset_button").addEventListener("click", resetGauge);
+// For testing purposes of simulating an action
+document
+  .querySelector("#execute_action")
+  .addEventListener("click", executeAction);
